730ecd0b84fc3fcec50c431b7f5480c9a7f353ca
sefin <spasenov2004@mail.ru>
2022-11-21 20:07:45 +0300

    Moved allocate array and fill

--- lab5/lab5.1/lab5.1/Header.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-int enterWithValidation();
-int** fillMatrix(int row, int col);
-void printMatrix(int** matrix, int row, int col);
-void checkElement(int** matrix, int row, int col);
--- lab5/lab5.222/lab 5.222/Header.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdbool.h>
-
-int** fillMatrix(int size);
-void printMatrix(int** matrix, int size);
-bool isSymmetric(int** arr, int size);
-void newArr(int** arr, int size, bool sim);
-int enterWithValidation(int a);
