1bb0deb8aa572fa9e4ff547b5039e9e930e3dc4e
sefin123 <spasenov2004@mail.ru>
2023-03-19 20:24:54 +0300

    Completed prepareWordsStruct function

--- lab2(sem 2)/lab2(sem 2)/Header.h
+++ lab2(sem 2)/lab2(sem 2)/Header.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <stdbool.h>
 
 typedef struct {
+	bool isEmpty;
 	char* name;
 	int size;
 	int count;
--- lab2(sem 2)/lab2(sem 2)/Main.c
+++ lab2(sem 2)/lab2(sem 2)/Main.c
@@ -1,75 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "Header.h"
 
 
+bool isLetter(char letter) {
+	return letter != ' ' && letter != '\n';
+}
+
+int getWordSize(char* string, int startIndex) {
+	int wordSize = 0;
+	for (int i = startIndex; isLetter(string[i]); i++) {
+		wordSize++;
+	}
+	return wordSize;
+}
+
+int findIndexWordInWordsStructByName(word* words, char* name, int wordCount) {
+	for (int i = 0; i < wordCount; i++)
+	{
+		if (!strcmp(words[i].name, name)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+
 word** prepareWordsStruct() {
-	// прочесть файл
 	FILE* file = fopen("file.txt", "r");
+	word* words = calloc(1, sizeof(word));
+	int wordsIterator = 0;
+
 	if (fopen == NULL) printf("file is open\n");
 	else printf("file isnt open\n");
+
 	char* str = calloc(256, sizeof(char));
 	while (fgets(str, 256, file)) {
+		int i = 0;
+		while (str[i] != '\n') {
+			while (!isLetter(str[i])) i++;
+		
+			int wordSize = getWordSize(str, i);
 
-	}
+			char* stringWord = calloc(wordSize, sizeof(char));
 
-	// заполнить массив структур
-	struct word* words = calloc(1, sizeof(word));
-	// вернить массив структур
-	return words ;
-}
-
-
-int isMeet(word* word,char* buf) {
-	int i = 0;
-	while (i != NULL) {
-		if (buf == word[i].word) {
-			word[i].count++;
-			return 1;
-		}
-		i++;
-	}
-	return 0;
-}
-
-void searchWord(char* str,word* words) {
-	int i = 0;
-	int wordsIterator = 0;
-	while (str[i] != '\n') {
-		while (str[i] == ' ' || str[i] == '\t' || str[i] == ',' || str[i] == '.' || str[i] == ':' || str[i] == ';') i++;
-		if (str[i] >= 'A' || str[i] <= "Z" || str[i] >= 'a' || str[i] <= 'z') {
-			int sizeOfWord = 0;
-			while (str[i] != ' ' && str[i] != '\t' && str[i] != ',' && str[i] != '.' && str[i] != ':' && str[i] != ';') sizeOfWord++;
-			words[wordsIterator].size = sizeOfWord;
-			char* buf = calloc(sizeOfWord, sizeof(char));
-			for (int j =  0,k = i; k < sizeOfWord, j < sizeOfWord; k++, j++) {
-				buf[j] = str[k];
-				if (isMeet(words, buf)) break;
-				words[wordsIterator].name = str[k];
+			for (int j = 0, k = i; k < wordSize - 1, j < wordSize - 1; k++, j++) {
+				stringWord[j] = str[k];
 			}
+
+			int existedWordIndex = findIndexWordInWordsStructByName(words, stringWord, wordsIterator);
+
+			if (existedWordIndex == -1) {
+				realloc(words, (wordsIterator + 1) * sizeof(word));
+				word newWord = { .isEmpty = false, .name = stringWord, .size = wordSize, .count = 0 };
+				words[wordsIterator] = newWord;
+				wordsIterator++;
+			}
+			else {
+				words[existedWordIndex].count = words[existedWordIndex].count + 1;
+			}
+			i = i + wordSize;
 		}
-		i++;
 	}
+	return words; 
 }
+
 
 int main() {
-
-	/*struct word *words = calloc(1, sizeof(word));
-
-	FILE* file;
-	int res = fopen_s(&file, "file.txt", "r+");
-	if (res == NULL) printf("file is open\n");
-	else printf("file isnt open\n");
-	char *str = calloc(256,sizeof(char));
-	while (fgets(str, 256, file)) {
-		printf(str);
-		searchWord(str,words);
-	}
-	printf("\n");
-	fclose(file);
-	if ((fclose(file)) != NULL) printf("file is close");
-	else printf("file isnt close");*/
-
-
+	word* words = prepareWordsStruct();
+	int i = 0;
+	while (!words[i].isEmpty) {
+		printf("name: %s", words[i].name);
+		printf("size: %d", words[i].size);
+		printf("count %d\n", words[i].count);
+			i++;
+	}	
 	return 0;
 }
