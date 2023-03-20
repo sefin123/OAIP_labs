a1f68f0d652d2b210eb86d50b5aea557bbfff391
sefin123 <spasenov2004@mail.ru>
2023-03-20 23:34:01 +0300

    Added read from file, prepared array of structures, added prototype to compose words

--- lab2(sem 2)/lab2(sem 2)/Header.h
+++ lab2(sem 2)/lab2(sem 2)/Header.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <stdbool.h>
 
-typedef struct {
-	char* name;
+typedef struct Word {
+	char changeTo[50];
+	char name[50];
+	int size;
 	int count;
-}word;
+	int value;
+	bool isChanged;
+} Word;
--- lab2(sem 2)/lab2(sem 2)/Main.c
+++ lab2(sem 2)/lab2(sem 2)/Main.c
@@ -4,105 +4,106 @@
 #include "Header.h"
 
 
-bool isLetter(char letter) {
-	return letter != ' ' && letter != '\n';
-}
-
-int getWordSize(char* string, int startIndex) {
-	int wordSize = 0;
-	for (int i = startIndex; isLetter(string[i]); i++) {
-		wordSize++;
-	}
-	return wordSize;
-}
-
-int findIndexWordInWordsStructByName(word* words, char* name, int wordCount) {
-	for (int i = 0; i < wordCount; i++) 
-	{
-		if (!strcmp(words[i].name, name)) {
-			words[i].count = words[i].count + 1;
-			return i;
-		}
+int findIndexByName(char* name, Word* words, int size) {
+	for (int i = 0; i < size; i++) {
+		if (!strcmp(words[i].name, name)) return i;
 	}
 	return -1;
 }
 
-int compare(const void* a, const void* b)
-{
-	
-	word* wordsA = (word*)a;
-	word* wordsB = (word*)b;
-	return ((wordsB->count) - (wordsA->count * wordsA->count));
-
+int compare(Word a, Word b) {
+	return a.size - b.size;
 }
 
-word** prepareWordsStruct(int *wordsIterator) {
-	int memorySize = 0;
-	FILE* file = fopen("file.txt", "r");
-	word* words = calloc(2 , sizeof(word));
-
-	//if (fopen == NULL) printf("file is open\n");
-	//else printf("file isnt open\n");
-
-	char* str = malloc(4096 * sizeof(char));
-	while (fgets(str, 4096, file)) {
-		int i = 0;
-		while (str[i] != '\n') {
-			while (!isLetter(str[i])) i++;
-		
-			int wordSize = getWordSize(str, i);
-
-			char* stringWord = calloc(wordSize + 1 , sizeof(char));
-			if (!stringWord) exit(1);
-			//qwe wqer qwe qwer qwer
-
-			for (int j = 0, k = i; j < wordSize; k++, j++) {
-				stringWord[j] = str[k];
-			}
-
-			int existedWordIndex = findIndexWordInWordsStructByName(words, stringWord, *wordsIterator);
-
-			if (existedWordIndex == -1) {
-				memorySize = memorySize + wordSize + 4;
-			
-				//words = realloc(words, memorySize);
-				
-				if(!words) exit(1);
-				
-				word newWord;
-				newWord.name = strdup(stringWord);
-
-				words[*wordsIterator] = newWord;
-				*wordsIterator = *wordsIterator + 1;
-			}
-			else {
-				//words[existedWordIndex].count = words[existedWordIndex].count + 1;
-			}
-			i = i + wordSize /* (-1???)*/;
-		}
-	}
-	return words; 
-}
-
-
+//слов должно быть больше одного
 //
 
 
 int main() {
-	int wordsIterator = 1;
-
-	word* words = prepareWordsStruct(&wordsIterator);
-
-	for (int i = 0; i<wordsIterator; i++) {
-		printf("name: %s ", words[i].name);
-		printf("count %d\n", words[i].count);
-	}
-
-	qsort(words, wordsIterator, sizeof(words[0]), compare);
+	Word words[256];
+	char stringInFile[256];
+	FILE* file = fopen("file.txt", "r");;
 	
-	for(int i = 0; i< wordsIterator; i++) {
-		printf("name: %s ", words[i].name);
-		printf("count %d\n", words[i].count);
-	}	
-	return 0;
+	char allWords[256][256];
+	int allWordsCounter = 0;
+	int stringIndex = 0;
+	while ((fgets(stringInFile, 256, file)) != NULL)
+	{
+		char word[256];
+		for (int i = 0; i <= (strlen(stringInFile)); i++)
+		{
+			// if space or NULL found, assign NULL into newString[ctr]
+			if (stringInFile[i] == ' ' || stringInFile[i] == '\0' || stringInFile[i] == '\n')
+			{
+				word[stringIndex] = '\0';
+
+				int wordIndex = findIndexByName(word, words, allWordsCounter);
+				if (wordIndex == -1) {
+					strcpy(words[allWordsCounter].name, word);
+					words[allWordsCounter].size = strlen(word);
+					words[allWordsCounter].count = 1;
+					words[allWordsCounter].isChanged = false;
+					words[allWordsCounter].value = strlen(word);
+					allWordsCounter++;
+				}
+				else {
+					words[wordIndex].count = words[wordIndex].count + 1;
+					words[wordIndex].value = words[wordIndex].value + strlen(word);
+				}
+				stringIndex = 0;    //for next word, init index to 0
+			}
+			else
+			{
+				word[stringIndex] = stringInFile[i];
+				stringIndex++;
+			}
+		}
+	}
+	/*for (int i = 0; i < allWordsCounter; i++) {
+		printf("name: %s -> size: %d count: %d\n", words[i].name, words[i].size, words[i].count);
+	}*/
+	qsort(words, allWordsCounter, sizeof(words[0]), compare);
+	int oldValueCount = 0;
+	printf("Sorted\n");
+	for (int i = 0; i < allWordsCounter; i++) {
+		oldValueCount = oldValueCount + words[i].value;
+		printf("name: %s -> size: %d count: %d totalSize: %d\n", words[i].name, words[i].size, words[i].count, words[i].value);
+	}
+	printf("Total value before compose: %d\n", oldValueCount);
+
+
+	for (int i = 0; i < allWordsCounter; i++) {
+		if (words[i].isChanged) continue;
+		
+		for (int j = 1; j < allWordsCounter; j++) {
+			if (words[j].isChanged) continue;
+
+			if (words[i].size != 0 && words[i].count > 1 && (words[i].value + words[j].value) >((words[i].size * words[j].count) + (words[j].size * words[i].count))) {
+				strcpy(words[i].changeTo, words[j].name);
+				strcpy(words[j].changeTo, words[i].name);
+
+				words[i].value = strlen(words[j].name) * words[i].count;
+				words[j].value = strlen(words[i].name) * words[j].count;
+				words[i].isChanged = true;
+				words[j].isChanged = true;
+				break;
+			}
+		}
+	}
+	int valueCount = 0;
+	printf("Changed\n");
+	for (int i = 0; i < allWordsCounter; i++) {
+		valueCount = valueCount + words[i].value;
+			printf("name: %s -> count:%d  new value: %d changeTo: %s\n", words[i].name, words[i].count, words[i].value, words[i].changeTo);
+	}
+	printf("Total value after compose: %d\n", valueCount);
+
+	fclose(file);
+
+
+	// Получить готовый массив структур check
+	// Отсортировать массив структур check
+	// Вызвавать decode пока размер меняется
+
+
 }
